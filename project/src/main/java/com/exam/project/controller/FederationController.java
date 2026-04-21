d9bc8e1b0cb05ef7c23f265cfd3697742cbebb2a
kenny0023 <hei.bryan.2@gmail.com>
2026-04-21 22:16:14 +0200

    YAML change and import

--- project/src/main/java/com/exam/project/controller/FederationController.java
+++ project/src/main/java/com/exam/project/controller/FederationController.java
@@ -1,5 +1,6 @@
 package com.exam.project.controller;
 
+import com.exam.project.service.FederationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
